5b2945374a58679f0ca51731104e8a7d8160f611
jiangnanxin <ss2280303225@163.com>
2020-05-09 18:18:21 +0800

    项目初始化

---
+++ src/main/java/com/jnx/springcloud/SpringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.jnx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jnx/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jnx.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
